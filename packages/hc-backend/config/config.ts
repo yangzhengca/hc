a9dbe53bd282fa56f8ea4ea1c030c9f86fe18293
yangzhengca <yangzhengca@gmail.com>
2023-02-08 22:20:49 -0700

    feat: Added sequelize

---
+++ packages/hc-backend/config/config.ts
@@ -0,0 +1,27 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+module.exports = {
+  development: {
+    username: process.env.DEV_DB_USER,
+    password: process.env.DEV_DB_PASSWORD,
+    database: process.env.DEV_DB_NAME,
+    host: process.env.DEV_DB_HOST,
+    port: process.env.DEV_DB_PORT,
+    dialect: process.env.DEV_DB_DIALECT,
+  },
+  test: {
+    username: 'root',
+    password: null,
+    database: 'database_test',
+    host: '127.0.0.1',
+    dialect: 'mysql',
+  },
+  production: {
+    username: 'root',
+    password: null,
+    database: 'database_production',
+    host: '127.0.0.1',
+    dialect: 'mysql',
+  },
+};
